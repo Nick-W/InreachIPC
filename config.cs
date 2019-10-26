8c203a63baff73dc7e1964010d057fb953fcd507
Nick-W <nick@oyo.co>
2019-10-25 19:09:11 -0600

    Initial commit

---
+++ config.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace InreachIPC
+{
+    /// <summary>
+    /// InReach Portal Credentials & Endpoint Settings
+    /// </summary>
+    public static class Config
+    {
+        public static string Username;
+        public static string Password;
+        internal static string customUri;
+
+        /// <summary>
+        /// Default region set to US
+        /// </summary>
+        public static RegionalEndpoints APIEndpoint = RegionalEndpoints.US;
+
+        /// <summary>
+        /// Regional endpoints as defined in the Delorme/Garmin IPC documentation
+        /// </summary>
+        /// <see cref="https://files.delorme.com/support/inreachwebdocs/IPC_Inbound.pdf"/>
+        /// <remarks>Custom defaults to "http://localhost", setting SetCustomApiUri will override this and automatically set the endpoint to Custom</remarks>
+        public enum RegionalEndpoints
+        {
+            [Region(Uri = "https://explore.garmin.com/IPCInbound/V1/")]
+            US,
+
+            [Region(Uri = "https://eur-enterprise.garmin.com/IPCInbound/V1/")]
+            Europe,
+
+            [Region(Uri = "https://explore.garmin.com/IPCInbound/V1/")]
+            Australia,
+
+            [Region(Uri = "http://localhost")]
+            Custom
+        }
+
+        /// <summary>
+        /// (Read Only) Uri of the InReach Portal API
+        /// </summary>
+        public static string GetEndpointUri => APIEndpoint == RegionalEndpoints.Custom ? customUri : ((Region)APIEndpoint.GetType().GetField(Enum.GetName(APIEndpoint.GetType(), APIEndpoint)).GetCustomAttributes(typeof(Region), true).SingleOrDefault()).Uri;
+
+        public static string SetCustomApiUri
+        {
+            set
+            {
+                APIEndpoint = RegionalEndpoints.Custom;
+                customUri = value;
+            }
+        }
+
+        public class Region : Attribute
+        {
+            public string Uri;
+        }
+    }
+}
---
+++ services.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json;
+
+namespace InreachIPC
+{
+    public class services
+    {
+        public async Task<HttpResponseMessage> Send(APIMessage model)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            using (var httpClient = new HttpClient(new HttpClientHandler() {Credentials = new NetworkCredential(Config.Username, Config.Password)}))
+            {
+                var modelAttribute = (APIMessage.ServicePath) model.GetType().GetCustomAttributes(typeof(APIMessage.ServicePath), true).SingleOrDefault();
+                switch (modelAttribute.method)
+                {
+                    case APIMessage.ServicePath.HttpMethods.POST:
+                        var payload = JsonConvert.SerializeObject(model,
+                            new JsonSerializerSettings()
+                            {
+                                NullValueHandling = NullValueHandling.Ignore,
+                                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                            });
+                        var postContent = new StringContent(payload, Encoding.UTF8, "application/json");
+                        response = await httpClient.PostAsync($"{Config.GetEndpointUri}/{modelAttribute.path}", postContent);
+                        break;
+                    case APIMessage.ServicePath.HttpMethods.GET:
+                        response = await httpClient.GetAsync($"{Config.GetEndpointUri}/{modelAttribute.path}");
+                        break;
+                }
+            }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new HttpRequestException(
+                    $"API request failed - code {response.StatusCode}: {response.Content.ReadAsStringAsync()}");
+
+            return response;
+        }
+
+        public class APIMessage
+        {
+            public class ServicePath : Attribute
+            {
+                public string path;
+                public HttpMethods method;
+
+                public enum HttpMethods
+                {
+                    POST,
+                    GET
+                }
+            }
+        }
+
+        public static class Messaging
+        {
+            /// <summary>
+            /// Model to send a binary (encrypted?) message
+            /// </summary>
+            [ServicePath(path = "Messaging.svc/Binary", method = ServicePath.HttpMethods.POST)]
+            public class BinaryMessageModel : APIMessage
+            {
+                [JsonProperty(PropertyName = "Messages")]
+                public List<Message> Messages = new List<Message>();
+
+                public class Message
+                {
+                    [JsonProperty(PropertyName = "Payload")]
+                    public byte[] Payload;
+
+                    [JsonProperty(PropertyName = "Recipients")]
+                    public List<long> Recipients = new List<long>();
+
+                    [JsonProperty(PropertyName = "Type")] public BinaryTypeModel Type;
+
+                    public enum BinaryTypeModel
+                    {
+                        Encrypted = 0,
+                        Generic = 1,
+                        EncryptedPinpoint = 2
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Model to send a text message, with optional reference data
+            /// </summary>
+            [ServicePath(path = "Messaging.svc/Message", method = ServicePath.HttpMethods.POST)]
+            public class TextMessageModel : APIMessage
+            {
+                [JsonProperty(PropertyName = "Messages")]
+                public List<Message> Messages = new List<Message>();
+
+                public class Message
+                {
+                    [JsonProperty(PropertyName = "Message")]
+                    public string MessageText;
+
+                    [JsonProperty(PropertyName = "Recipients")]
+                    public List<long> Recipients = new List<long>();
+
+                    [JsonProperty(PropertyName = "Sender")]
+                    public string Sender;
+
+                    [JsonProperty(PropertyName = "Timestamp")]
+                    public DateTime Timestamp;
+
+                    [JsonProperty(PropertyName = "ReferencePoint")]
+                    public ReferencePointModel ReferencePoint;
+
+
+                    public class ReferencePointModel
+                    {
+                        [JsonProperty(PropertyName = "Altitude")]
+                        public long Altitude;
+
+                        [JsonProperty(PropertyName = "Coordinate")]
+                        public CoordinateModel Coordinate = new CoordinateModel();
+
+                        [JsonProperty(PropertyName = "Course")]
+                        public long Course;
+
+                        [JsonProperty(PropertyName = "Label")] public string Label;
+
+                        [JsonProperty(PropertyName = "LocationType")]
+                        public LocationTypes LocationType;
+
+                        [JsonProperty(PropertyName = "Speed")] public long Speed;
+
+                        public class CoordinateModel
+                        {
+                            public long Latitude;
+                            public long Longitude;
+                        }
+
+                        public enum LocationTypes
+                        {
+                            ReferencePoint = 0,
+                            GPSLocation = 1
+                        }
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Model to retrieve the API Version
+            /// </summary>
+            [ServicePath(path = "Messaging.svc/Version", method = ServicePath.HttpMethods.GET)]
+            public class VersionModel : APIMessage
+            {
+            }
+        }
+    }
+}
