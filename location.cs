5cb526c501031b5b5d220f00689e3954bae1df0b
Greta Parks <gretaparks@gmail.com>
2020-01-20 14:18:33 -0700

    Completes LastKnownLocation

--- location.cs
+++ location.cs
@@ -11,44 +11,34 @@ namespace Ayva.InreachIPC
         [Services.APIMessage.ServicePath(path = "Location.svc/LastKnownLocation?IMEI={PARAM}", method = Services.APIMessage.ServicePath.HttpMethods.POST)]
         public class LastKnownLocation: Services.APIMessage
         {
-
+            public class CoordinateModel
+            {
+                public long Latitude;
+                public long Longitude;
+            }
+            
             [JsonProperty(PropertyName = "Altitude")]
             public long Altitude;
 
-            /*[JsonProperty(PropertyName = "Coordinate")]
-            public ;*/
+            [JsonProperty(PropertyName = "Coordinate")]
+            public CoordinateModel Coordinate = new CoordinateModel();
 
             [JsonProperty(PropertyName = "Course")]
             public long Course;
-/*
+
             [JsonProperty(PropertyName = "GPSFixStatus")]
-            //public*/
+            public int GPSFix;
+            //No Fix = 0, 2D Fix = 1, 3D Fix = 2, 3D Fix+ = 3
 
             [JsonProperty(PropertyName = "IMEI")]
-            public long IMEI;
+            public string IMEI;
 
             [JsonProperty(PropertyName = "Speed")]
             public long Speed;
 
             [JsonProperty(PropertyName = "Timestamp")]
-            //public 
-
-            /*{
-	"Locations":[{
-		"Coordinate":{
-			"Latitude":1.26743233E+15,
-			"Longitude":1.26743233E+15
-		},
-		"GPSFixStatus":0,
-
-		"Timestamp":"\/Date(928149600000+0000)\/"
-	}]
-}
-*/
-
+            public DateTime Timestamp;
         }
-
-
     }
 
 }
